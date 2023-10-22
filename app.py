09be3cf5195a882c40ef44884e98b8aa08d7e9ad
milix1953 <79617515+milix1953@users.noreply.github.com>
2023-10-22 15:21:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ from linebot.models import *
 #======python的函數庫==========
 import tempfile, os
 import datetime
-import openai
 import time
 #======python的函數庫==========
+
+#======這裡是呼叫的檔案內容=====
+from mongodb_function import *
+#======這裡是呼叫的檔案內容=====
 
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
@@ -21,17 +24,6 @@ static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 line_bot_api = LineBotApi(os.getenv('wNeecpSbD4LVSh7qG+7lP5dKy4ws9S/BxvNU5bgDn4ufCtkK6ipJzH6xE8XwTJkXLsKKGmI7cz+TtbCctuxMiUUTvCCD0w1s1rpWA3OV0h0Q0SoKKUNkHapkPH2xWho2WaAEuWLVfhUhT6I8g6IpvwdB04t89/1O/w1cDnyilFU='))
 # Channel Secret
 handler = WebhookHandler(os.getenv('e9ec4cc1141ece77dd6173d82037195c'))
-# OPENAI API Key初始化設定
-openai.api_key = os.getenv('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')
-
-
-# def GPT_response(text):
-#     # 接收回應
-#     response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
-#     print(response)
-#     # 重組回應
-#     answer = response['choices'][0]['text'].replace('。','')
-#     return answer
 
 
 # 監聽所有來自 /callback 的 Post Request
@@ -41,6 +33,7 @@ def callback():
     signature = request.headers['X-Line-Signature']
     # get request body as text
     body = request.get_data(as_text=True)
+    write_one_data(eval(body.replace('false','False')))
     app.logger.info("Request body: " + body)
     # handle webhook body
     try:
@@ -49,19 +42,19 @@ def callback():
         abort(400)
     return 'OK'
 
-
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    # GPT_answer = GPT_response(msg)
-    # print(GPT_answer)
-    # line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
 
 @handler.add(PostbackEvent)
 def handle_message(event):
     print(event.postback.data)
-
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    postback_data = event.postback.data  # 取得 postback 資料
+    reply_message = "You selected: " + postback_data
+    line_bot_api.reply_message(event.reply_token, TextMessage(text=reply_message))
 
 @handler.add(MemberJoinedEvent)
 def welcome(event):
